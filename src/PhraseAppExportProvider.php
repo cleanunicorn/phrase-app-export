ec6df7fba520ad4c96367bbf59a974b4bd0c4961
Daniel Luca <daniel.luca@elasticorange.com>
2016-09-12 13:08:21 +0300

    Initial commit. Add service provider

---
+++ src/PhraseAppExportProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hydrarulz\PhraseAppExport;
+
+use Illuminate\Support\ServiceProvider;
+
+class PhraseAppExportProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
